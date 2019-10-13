417d518a710b0ef92dbbc261ae71833082d6586e
Kert Prink <prinkkert@gmail.com>
2019-10-13 14:43:12 +0300

    TDD first iteration - development

--- src/main/java/tartu1nobe/backend/VechileData/VechileDataReader.java
+++ src/main/java/tartu1nobe/backend/VechileData/VechileDataReader.java
@@ -1,9 +1,23 @@
 package tartu1nobe.backend.VechileData;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.stream.Collectors;
+
 public class VechileDataReader {
 
     public static String readFromFile(String filename) {
+        File file = new File(filename);
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            StringBuilder sb = new StringBuilder();
 
-        return "todo functionality";
+            return br.lines().map(sb::append).collect(Collectors.joining());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return "";
     }
 }

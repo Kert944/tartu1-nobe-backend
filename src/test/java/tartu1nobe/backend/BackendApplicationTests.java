4ab353d4b69e5210767d16ee06bfa26efd9377f8
Miron Storozev <miron.storozhev96@gmail.com>
2019-10-13 18:52:04 +0300

    TDD fourth iteration - test

--- src/test/java/tartu1nobe/backend/BackendApplicationTests.java
+++ src/test/java/tartu1nobe/backend/BackendApplicationTests.java
@@ -7,6 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import tartu1nobe.backend.VechileData.VechileDataReader;
 
+import java.io.FileNotFoundException;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BackendApplicationTests {
@@ -33,6 +35,14 @@ public class BackendApplicationTests {
         String result = VechileDataReader.getVechileBatteryPercentage(VechileDataReader.readFromFile(fileName));
 
         Assert.assertEquals("90%", result);
+    }
+
+    @Test
+    public void givenNoFile_emptyStringIsReturned() {
+        String nonExistantFilePath = "src/test-data/non-existent-file";
+        Assert.assertEquals("", VechileDataReader.readFromFile(nonExistantFilePath));
+        Assert.assertEquals("", VechileDataReader.getVechileId(nonExistantFilePath));
+        Assert.assertEquals("", VechileDataReader.getVechileBatteryPercentage(nonExistantFilePath));
     }
 
 }

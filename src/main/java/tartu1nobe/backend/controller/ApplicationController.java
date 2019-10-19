220963bc86c7269130c22fb4fe0c97bc11d21d5f
Kert Prink <prinkkert@gmail.com>
2019-10-19 12:36:30 +0300

    FE - BE connection

--- src/main/java/tartu1nobe/backend/controller/ApplicationController.java
+++ src/main/java/tartu1nobe/backend/controller/ApplicationController.java
@@ -1,12 +1,14 @@
 package tartu1nobe.backend.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class ApplicationController {
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot team Tartu1!";
+        return "Message from Spring Boot backend by team Tartu1!";
     }
 }

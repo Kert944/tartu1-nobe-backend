7b93c7133c9fc49f7177c0e5ca32f2137d3aa661
Hanna Tagen <hanna.tagen@gmail.com>
2019-10-22 01:12:50 +0300

    added /car endpoint

--- src/main/java/tartu1nobe/backend/controller/ApplicationController.java
+++ src/main/java/tartu1nobe/backend/controller/ApplicationController.java
@@ -4,11 +4,16 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin("http://localhost:4200")
 @RestController
 public class ApplicationController {
     @RequestMapping("/")
     public String index() {
         return "Message from Spring Boot backend by team Tartu1!";
+    }
+
+    @RequestMapping("/car/nob001")
+    public String car() {
+        return "Battery percentage: 90%";
     }
 }
